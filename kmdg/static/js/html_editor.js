58eb04bd5e89360ea014e9db2be8e36aed91ab2d
skoczek192@gmail.com <gh+B1voJzk13UeXR>
2017-10-13 20:12:48 +0200

    Workaround for  the resizing of CKE widget problem.

--- kmdg/static/js/html_editor.js
+++ kmdg/static/js/html_editor.js
@@ -1,8 +1,14 @@
 (function($) {
     $(document).ready(function($) {
-		CKEDITOR.replace('id_content');
+        setTimeout(function(){
+            var wdg = document.getElementsByClassName("django-ckeditor-widget")[0];
+            var txt = document.getElementById("id_content");
+            var cke = document.getElementById("cke_id_content"); // cke div
+            var elements = [wdg, txt, cke];
+
+            for(var idx = 0; idx < elements.length; idx++) {
+                elements[idx].style.width = "100%";
+            }
+        }, 1000);
     });
 })(django.jQuery);
-
-
-
